19fb034ae84ab8ace30ccfa0093c1583502a385d
Sourodip Roy <124880948+Sourodip-1@users.noreply.github.com>
2025-06-07 16:23:11 +0530

    Update middleware.mjs

--- NEXTRA DIGITAL/middleware.mjs
+++ NEXTRA DIGITAL/middleware.mjs
@@ -1,12 +1,12 @@
-// middleware.mjs
+// middleware.js
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-  const url = request.nextUrl.clone();
   const token = request.cookies.get('admin-auth')?.value;
 
-  if (url.pathname.startsWith('/dashboard')) {
+  if (request.nextUrl.pathname.startsWith('/dashboard')) {
     if (token !== process.env.NEXT_PUBLIC_ADMIN_TOKEN) {
+      const url = request.nextUrl.clone();
       url.pathname = '/login';
       return NextResponse.redirect(url);
     }
@@ -16,5 +16,5 @@ export function middleware(request) {
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*'],
+  matcher: ['/dashboard'],
 };
